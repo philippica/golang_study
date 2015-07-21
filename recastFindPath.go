bf3d2b3027d0b7fdeaaaf21bb2940c95fd02245c
philippica <429844547@qq.com>
2015-07-21 11:02:08 +0800

    Update recastFindPath.go

--- recastFindPath.go
+++ recastFindPath.go
@@ -5,6 +5,9 @@ import "math"
 
 
 
+const DT_FAILURE = 1 << 31
+const DT_SUCCESS = 1 << 30
+const DT_INVALID_PARAM = 1 << 3
 type dtNode struct {
 	pos   []float64
 	cost  float64
@@ -14,16 +17,49 @@ type dtNode struct {
 	id    uint64
 }
 
+//todo : to create a priority_queue
+
 type openList struct {
 	m_size int
+	m_heap []dtNode
 }
 
-func (instance *openList) push(u *dtNode){
-	
+func (instance *openList) trickleDown(i int, node *dtNode){
+	child := (i * 2) + 1
+	for(child < instance.m_size){
+		if child + 1 < instance.m_size && instance.m_heap[child].total > instance.m_heap[child + 1].total{
+			child++;
+		}	
+		instance.m_heap[i] = instance.m_heap[child]
+		i = child
+		child = i * 2 + 1
+	}
 }
+
+
+
 func (instance *openList) pop()*dtNode{
-	
+	result := instance.m_heap[0]
+	instance.m_size--;
+	return &result
 }
+
+
+func (instance *openList) bubbleUp(i int, node *dtNode){
+	parent := (i - 1) >> 1;
+	for((i > 0) && instance.m_heap[parent].total > node.total){
+		instance.m_heap[i] = instance.m_heap[parent];
+		i = parent;
+		parent = (i - 1) >> 1
+	}
+	instance.m_heap[i] = *node
+}
+
+func (instance *openList) push(node *dtNode){
+	instance.m_size++;
+	instance.bubbleUp(instance.m_size - 1, node)
+}
+
 
 func (instance *openList) empty()bool{
 	if instance.m_size == 0{
@@ -44,23 +80,23 @@ func findPath(startRef uint64, endRef uint64, startPos *[]float64, endPos *[]flo
 	*pathCount = 0
 
 	if startRef == 0 || endRef == 0 {
-		return 2147483656
+		return DT_FAILURE | DT_INVALID_PARAM
 	}
 
 	if maxPath == 0 {
-		return 2147483656
+		return DT_FAILURE | DT_INVALID_PARAM
 	}
-
+/*
 	if !validRef(startRef) || !validRef(endRef) {
-		return 2147483656
+		return DT_FAILURE | DT_INVALID_PARAM
 	}
-
+*/
 	if startRef == endRef {
 		*path = append(*path, startRef)
 		*pathCount = 1
-		return 1073741824
+		return DT_SUCCESS
 	}
-	var m_nodePool nodePool
+	//var m_nodePool nodePool
 	m_openList := &openList{}
 
 	startNode := new(dtNode)
